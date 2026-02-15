5f4791f5754eb456cc76323183df996d78ca98ed
sunilsala88 <sunilsala88@gmail.com>
2026-02-15 17:05:41 +0530

    feb-15-1

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ import time
 logging.info("logging first Strategy execution started")
 
 strategy_name = "supertrend_ema_strategy"
-logging.basicConfig(level=logging.INFO, filename=f"{strategy_name}.log",filemode='a',format="%(asctime)s - %(message)s")
-logging.getLogger('ib_async').setLevel(logging.CRITICAL)
-logging.info("logging second Strategy execution started")
+print(strategy_name,'started')
 while True:
     # if dt.datetime.now() > end_time:
     #     break
     ct = dt.datetime.now()
-    logging.info(f"log time Current time: {ct}")
     print(f" print time Current time: {ct}")
     time.sleep(60)
