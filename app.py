e329f8cd3de99673133ba50ac0bc5ac552031133
sunilsala88 <sunilsala88@gmail.com>
2026-02-15 18:19:42 +0530

    abc

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 
-print('print first list')
-
-import datetime as dt
 import logging
 import time
-logging.info("logging first Strategy execution started")
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+print('print first list')
+
+logging.info("logging first Strategy execution started")
+print('this line is written in wizzer')
 strategy_name = "supertrend_ema_strategy"
-print(strategy_name,'started')
+print(strategy_name, 'started')
+
+# Get current UTC time
+dt_utc = datetime.now(ZoneInfo("UTC"))
+
+# Convert to Indian Standard Time
+dt_ist = dt_utc.astimezone(ZoneInfo("Asia/Kolkata"))
+
+print("Indian Standard Time:", dt_ist)
+
+
 while True:
-    # if dt.datetime.now() > end_time:
+    # if datetime.now() > end_time:
     #     break
-    ct = dt.datetime.now()
+    dt_utc = datetime.now(ZoneInfo("UTC"))
+    ct = dt_utc.astimezone(ZoneInfo("Asia/Kolkata"))
     print(f" print time Current time: {ct}")
     time.sleep(60)
